823793a0e891a9b5c7c0c2873044fa4e3aaf2b6b
BisaDev <bisa.fernandez@blueriver.com>
2021-10-20 06:59:59 -0500

    Updates validations

--- server/helpers/csv-service.js
+++ server/helpers/csv-service.js
@@ -1,4 +1,6 @@
 import fs from "fs";
+import Patient from "../patient/patient.model"
+import Email from "../email/email.model"
 
 export function getCleanupData(numColumns= 16) {
 	const textData = fs.readFileSync("/Users/abisaid.fernandez/Downloads/inputData.txt", "utf8");
@@ -9,7 +11,7 @@ export function getCleanupData(numColumns= 16) {
 }
 
 export async function processData () {
-
+	let newEmails
 	const numColumns = 16
 	const {cleanupData = [] , columns = []} = getCleanupData(numColumns)
 
@@ -29,7 +31,36 @@ export async function processData () {
 		columnIndex ++
 	}
 
-	function handleInsertOrUpdate(item) {
+	async function handleInsertOrUpdate(item) {
+		const isPatientInDb = await Patient.findOne({ "Card Number": item["Card Number"] }, function (err, doc){
+			if(!doc) return
+			doc["Program Identifier"]= 	item["Program Identifier"] || null,
+			doc["Data Source"]=  		item["Data Source"] || null,
+			doc["Card Number"]= 		item["Card Number"] || null,
+			doc["Member ID"]= 			item["Member ID"] || null,
+			doc["First Name"]= 			item["First Name"] || null,
+			doc["Last Name"]=  			item["Last Name"] || null,
+			doc["Date of Birth"]=  		item["Date of Birth"] || null,
+			doc["Address 1"]=  			item["Address 1"] || null,
+			doc["Address 2"]= 			item["Address 2"] || null,
+			doc["City"]= 				item["City"] || null,
+			doc["State"]= 				item["State"] || null,
+			doc["Zip code"]= 			item["Zip code"] || null,
+			doc["Telephone number"]= 	item["Telephone number"] || null,
+			doc["Email Address"]=  		item["Email Address"] || null,
+			doc["CONSENT"]= 			item["CONSENT"] || null,
+			doc["Mobile Phone"]=		item["Mobile Phone"] || null,
+			doc.save();
+		  });
+		
+		// // console.log("isPatientInDb", isPatientInDb)
+		if(!isPatientInDb) {
+			processedCsvToInsert.push(item)
+		} else {
+			const itemToUpdate = item
+			item["_id"] = isPatientInDb["_id"]
+			processedCsvToUpdate.push(itemToUpdate)
+		}
 		if(item["CONSENT"] === "Y" && item["Email Address"]) {
 			patientsThatGaveConsent.push(item)
 		}
@@ -56,12 +87,35 @@ export async function processData () {
 		}
 	}
 
+	
+	if(patientsThatGaveConsent.length) {
+		const emailPayload = []
+		const frequency = ["Day 1", "Day 2", "Day 3", "Day 4"]
+		const dayInMs = 86400000
+		patientsThatGaveConsent.map( patient => {
+			frequency.map ( (day, index) => {
+				const current = new Date()
+				const correspondingDate = new Date(current.getTime() + (dayInMs * (index + 1)))
+				const email = {
+					"name": day,
+					"scheduled_date": correspondingDate,
+					"locator": patient["Email Address"] || patient["Card Number"]
+				}
+				emailPayload.push(email)
+			})
+			
+		})
+		newEmails = await Email.insertMany(emailPayload)
+		// console.log("newEmails", newEmails)
+	}
+
 	return {
 		processedCsvToInsert,
 		processedCsvToUpdate,
 		patientsThatGaveConsent,
 		missingNamePatients,
 		missingEmailPatients,
+		newEmails
 	} 
 
 }
--- tests/server/express.test.js
+++ tests/server/express.test.js
@@ -1,40 +1,63 @@
-import _ from "lodash"
-import { getCleanupData, processData } from '../../server/helpers/csv-service';
+import _ from "lodash";
+import mongoose from "mongoose";
+import config from "../../server/config/config"
+import { getCleanupData, processData } from "../../server/helpers/csv-service";
 
-describe('Tests the reading CSV tool', () => {
-  test('Incoming CSV data should match with the current setup',  () => {
-    const dataToMatch = [
-      'Program Identifier', 'Data Source',
-      'Card Number',        'Member ID',
-      'First Name',         'Last Name',
-      'Date of Birth',      'Address 1',
-      'Address 2',          'City',
-      'State',              'Zip code',
-      'Telephone number',   'Email Address',
-      'CONSENT',            'Mobile Phone'
-    ]
-    const fileData = getCleanupData()	
-    const { columns} = fileData
-    const dataMatches = _.isEqual(dataToMatch,columns)
-    expect(dataMatches).toBeTruthy();
+beforeAll(async () => {
+	mongoose.Promise = Promise;
 
-    //expect(foo).toBe(404);
-    //expect(status).toBe(404);
-    //expect(data).toHaveProperty('msg');
-    //expect(data.msg).toContain('404');
-    // done();
-  });
-  test('Should catch patients with no First Name registered', async () => {
-    const fileData = await processData()	
-    const { missingNamePatients = [] } = fileData
-    console.log("missingNamePatients", missingNamePatients)
-    expect(missingNamePatients.length).toBeTruthy();
-  });
-  test('Should catch patients with no Email registered, but consent is set to Y', async () => {
-    const fileData = await processData()	
-    const { missingEmailPatients = [] } = fileData
-    console.log("missingEmailPatients", missingEmailPatients)
-    expect(missingEmailPatients.length).toBeTruthy();
-  });
+	// connect to mongo db
+	const mongoUri = config.mongo.host;
+	mongoose.connect(mongoUri, { useNewUrlParser: true });
 });
+jest.setTimeout(60000);
 
+let fileData;
+
+describe("Tests the reading CSV tool", () => {
+	test("Incoming CSV data should match with the current setup", () => {
+		const dataToMatch = [
+			"Program Identifier",
+			"Data Source",
+			"Card Number",
+			"Member ID",
+			"First Name",
+			"Last Name",
+			"Date of Birth",
+			"Address 1",
+			"Address 2",
+			"City",
+			"State",
+			"Zip code",
+			"Telephone number",
+			"Email Address",
+			"CONSENT",
+			"Mobile Phone"
+		];
+		fileData = getCleanupData();
+		const { columns } = fileData;
+		const dataMatches = _.isEqual(dataToMatch, columns);
+		expect(dataMatches).toBeTruthy();
+	});
+	test("Should catch patients with no First Name registered", async () => {
+		const fileData = await processData();
+		const { missingNamePatients = [] } = fileData;
+		console.log("missingNamePatients", missingNamePatients);
+		expect(missingNamePatients.length).toBeTruthy();
+	});
+
+	test("Should catch patients with no Email registered, but consent is set to Y", async () => {
+		const fileData = await processData();
+		const { missingEmailPatients = [] } = fileData;
+		console.log("missingEmailPatients", missingEmailPatients);
+		expect(missingEmailPatients.length).toBeTruthy();
+	});
+
+	test("When finished, Email entries should have been been created", async () => {
+		const fileData = await processData();
+		const { newEmails = [] } = fileData;
+		console.log("newEmails", newEmails);
+		expect(newEmails.length).toBeTruthy();
+	});
+
+});
